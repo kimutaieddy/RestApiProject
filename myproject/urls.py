387d054578dda295e4eae6906befe85e54098018
Your Name <eddykimutai@outlook.com>
2024-09-17 13:30:01 +0300

    Refactor myproject/urls.py to include rest_framework.urls and admin.site.urls

--- myproject/urls.py
+++ myproject/urls.py
@@ -25,6 +25,6 @@ router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('api-auth/', Home.as_view(), name='home'),
-    path('', Home.as_view(), name='home'),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('admin/', admin.site.urls),
 ]
